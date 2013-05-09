1d222b76bd87fb487bd37e53c85d51e83bb4eb40
Douglas Romines <DrRomines@gmail.com>
2013-05-08 19:05:28 -0600

    Changed Board's toString method
    prints out the board EX: (xox),(xxo),(oox)

--- tttgame-api/src/main/java/edu/neumont/csc380/models/Board.java
+++ tttgame-api/src/main/java/edu/neumont/csc380/models/Board.java
@@ -1,11 +1,40 @@
 package edu.neumont.csc380.models;
 
+import javax.xml.bind.annotation.XmlElement;
+
 public class Board 
 {
-	String[][] board = new String[3][3];
+	@XmlElement(name="board")
+	private String[][] board;
 	
-	public Board(String[][] board)
+	public Board()
 	{
+		board = new String[3][3];
+	}
+	
+	public String[][] getBoard() {
+		return board;
+	}
+	
+	public void setBoard(String[][] board) {
 		this.board = board;
+	}
+	
+	@Override
+	public String toString()
+	{
+		String result = "";
+		// Example toString(): (xox),(xxo),(oox)
+		for(int row = 0; row < board.length; row++) {
+			result += "(";
+			for(int column = 0; column < board.length; column++) {
+				result += board[row][column];
+			}
+			result += "),";
+		}
+		// Take out the extra comma at the end of result
+		result.substring(result.length()).replace(",", "");
+		
+		return result;
 	}
 }
